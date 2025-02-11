b74e5e467c0eeb669799ba31c0489a6b48d88258
nayangodhwani-atri <ngodhwan@atrihub.io>
2025-02-11 12:35:56 -0800

    test:1816 Created the postgresql database

--- django/helloWorld/helloWorld/urls.py
+++ django/helloWorld/helloWorld/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from polls.views import *
 urlpatterns = [
+    path('hello',index,name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- django/helloWorld/polls/views.py
+++ django/helloWorld/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Created my own view!")
 
 # Create your views here.
 
