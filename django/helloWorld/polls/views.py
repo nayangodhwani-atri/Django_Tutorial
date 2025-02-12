f251c87042f25430584953769d2d848354814ea2
nayangodhwani-atri <ngodhwan@atrihub.io>
2025-02-12 11:36:16 -0800

    feat: Views and Templates, Raise 404
    Created a detail.html file to raise 404 error
    
    [ICTRAINING 1819]

--- django/helloWorld/polls/views.py
+++ django/helloWorld/polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:1]
@@ -10,7 +12,11 @@ def index(request):
     return render(request,"polls/index.html")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
