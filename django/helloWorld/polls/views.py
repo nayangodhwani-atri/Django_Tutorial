897a086f7fe2ec3028d1a5dd9b0447c39251e742
nayangodhwani-atri <ngodhwan@atrihub.io>
2025-02-12 12:40:27 -0800

    test: Added test case, Created two test cases
    [ictraining 1821]

--- django/helloWorld/polls/views.py
+++ django/helloWorld/polls/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import render
 from .models import Choice, Question
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:1]
@@ -45,5 +47,25 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+     def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 
